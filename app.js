559604dffe48d10fa121377923a3bd1852931ca1
vincenthanguk <vincentkreusel@gmail.com>
2021-06-08 17:50:36 +0200

    added config.env, development and production scripts

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// NOTE: -------- MIDDLEWARES --------
-
-app.use(morgan('dev'));
+// -------- MIDDLEWARES --------
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
@@ -21,16 +22,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// -------- HANDLERS (/Controllers) --------
-
-// -------- ROUTES --------
-// app.get('/api/v1/decks', getAllDecks);
-// app.post('/api/v1/decks', createDeck);
-// app.get('/api/v1/decks/:id', getDeck);
-// app.patch('/api/v1/decks/:id', updateDeck);
-// app.delete('/api/v1/decks/:id', deleteDeck);
-// --- slicker and more readable version below ---
 
 // mounting the router (-> apply deckRouter middleware and userRouter middleware respectively)
 app.use('/api/v1/decks', deckRouter);
